5c60d83b00b3aad3722f8c02bd6c3ada9d95740c
deividas.kvietkauskas <deividas.kvietkauskas@bs2.lt>
2022-12-10 00:16:21 +0200

    DMS-31 added endpoints for deleteting student

--- src/main/java/com/dms/dmsapplication/controllers/ApiController.java
+++ src/main/java/com/dms/dmsapplication/controllers/ApiController.java
@@ -3,6 +3,7 @@ package com.dms.dmsapplication.controllers;
 import com.dms.dmsapplication.exception.ResourceNotFoundException;
 import com.dms.dmsapplication.models.User;
 import com.dms.dmsapplication.repository.UserRepository;
+import com.dms.dmsapplication.repository.UserRoomRepository;
 import com.dms.dmsapplication.security.services.UserDetailsImpl;
 import com.dms.dmsapplication.security.services.UserDetailsServiceImpl;
 import com.dms.dmsapplication.service.UserRoomsService;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,13 +32,17 @@ public class ApiController {
 
     private final UserRoomsService userRoomsService;
 
+    private final UserRoomRepository userRoomRepository;
+
     private final UserDetailsServiceImpl userDetailsService;
 
     private final UserRepository userRepository;
 
-    public ApiController(UserRoomsService userRoomsService, UserDetailsServiceImpl userDetailsService,
+    public ApiController(UserRoomsService userRoomsService, UserRoomRepository userRoomRepository,
+            UserDetailsServiceImpl userDetailsService,
             UserRepository userRepository) {
         this.userRoomsService = userRoomsService;
+        this.userRoomRepository = userRoomRepository;
         this.userDetailsService = userDetailsService;
         this.userRepository = userRepository;
     }
@@ -99,5 +105,21 @@ public class ApiController {
         userRepository.save(user);
 
         return ResponseEntity.ok().body(user);
+    }
+
+    @DeleteMapping("students/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteStudent(@PathVariable(value = "id") long userId) throws ResourceNotFoundException {
+        userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+
+        if (userRoomRepository.findByUserId(userId).isEmpty())
+        {
+            userRepository.deleteById(userId);
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.badRequest().body("User can't be deleted because of userStatus");
+        }
     }
 }
