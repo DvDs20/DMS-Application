b370adc0c9701d93f6ce3873b8c2faba829c1c33
deividas.kvietkauskas <deividas.kvietkauskas@bs2.lt>
2022-12-19 20:31:19 +0200

    DMS-44 added contract view functionality for student

---
+++ src/main/java/com/dms/dmsapplication/contracts/payload/response/ResponseForUserWithoutContract.java
@@ -0,0 +1,15 @@
+package com.dms.dmsapplication.contracts.payload.response;
+
+public class ResponseForUserWithoutContract {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
--- src/main/java/com/dms/dmsapplication/contracts/repository/ContractsRepository.java
+++ src/main/java/com/dms/dmsapplication/contracts/repository/ContractsRepository.java
@@ -4,7 +4,9 @@ import com.dms.dmsapplication.contracts.models.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContractsRepository extends JpaRepository<Contract, Long> {
-
+    Optional<Contract> findByStudentId(Long studentId);
 }
