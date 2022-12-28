ff01bc5665c793a370112c638277df61be30e79f
deividas.kvietkauskas <deividas.kvietkauskas@bs2.lt>
2022-12-28 19:28:00 +0200

    DMS-61 added methods and endpoints for parcel functionality

--- src/main/java/com/dms/dmsapplication/parcels/repository/ParcelRepository.java
+++ src/main/java/com/dms/dmsapplication/parcels/repository/ParcelRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ParcelRepository extends JpaRepository<Parcel, Long> {
-
+    Optional<Parcel> findByStudentId(long studentId);
 }
