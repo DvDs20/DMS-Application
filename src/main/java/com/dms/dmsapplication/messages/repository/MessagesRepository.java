61aeb04f31fd913a374ebfbcb247fe8227ad4e67
deividas.kvietkauskas <deividas.kvietkauskas@bs2.lt>
2023-01-03 22:02:22 +0200

    fixed bug

--- src/main/java/com/dms/dmsapplication/messages/repository/MessagesRepository.java
+++ src/main/java/com/dms/dmsapplication/messages/repository/MessagesRepository.java
@@ -3,6 +3,10 @@ package com.dms.dmsapplication.messages.repository;
 import com.dms.dmsapplication.messages.models.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MessagesRepository extends JpaRepository<Message, Long> {
+
+    Optional<Message> findByStudentId(Long studentId);
 
 }
